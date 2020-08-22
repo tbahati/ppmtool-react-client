15782e53fc0655f3606b6a78b38b5d5db819ff7e
tbahati <tuyijean2003@gmail.com>
2020-08-22 21:11:32 +0200

    Test

--- src/actions/projectActions.js
+++ src/actions/projectActions.js
@@ -4,7 +4,7 @@ import { GET_ERRORS } from "./types";
 export const createProject = (project, history) => async dispatch => {
     try {
         const res = await axios.post("http://localhost:8080/api/project", project)
-        history.push("/dashboard")
+        history.push("/dashboard");
     } catch (err) {
         dispatch({
             type: GET_ERRORS,
