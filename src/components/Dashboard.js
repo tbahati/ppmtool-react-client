0973f91d662ba3a7adb6648987c4b5da8bd0923a
tbahati <tuyijean2003@gmail.com>
2020-08-23 17:06:08 +0200

    display Products on Dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'
 import ProjectItem from './Project/ProjectItem'
 import CreateProjectButton from './Project/CreateProjectButton'
+import { connect } from "react-redux"
+import { getProjects } from "../actions/projectActions"
+import PropTypes from "prop-types"
 
 class Dashboard extends Component {
+
+    componentDidMount() {
+        this.props.getProjects();
+    }
+
     render() {
+
+        const { projects } = this.props.project;
         return (
             <div className="projects">
                 <div className="container">
@@ -11,15 +21,13 @@ class Dashboard extends Component {
                         <div className="col-md-12">
                             <h1 className="display-4 text-center">Projects</h1>
                             <br />
-                           
-<CreateProjectButton />
 
-
-
-                            <br />
-                            <hr />
-                            <ProjectItem />
-
+                            <CreateProjectButton />
+                            {
+                                projects.map(project => (
+                                    <ProjectItem key={project.id} project={project} />
+                                ))
+                            }
                         </div>
                     </div>
                 </div>
@@ -28,4 +36,16 @@ class Dashboard extends Component {
     }
 }
 
-export default Dashboard
+Dashboard.propTypes = {
+    project: PropTypes.object.isRequired,
+    getProjects: PropTypes.func.isRequired
+}
+
+const mapStateToProps = state => ({
+    project: state.project
+})
+
+export default connect(
+    mapStateToProps,
+    { getProjects }
+)(Dashboard)
